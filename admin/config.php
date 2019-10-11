d3454563c8816474788c2af24d86084148861e89
Ubuntu <ubuntu@ip-172-31-45-41.us-east-2.compute.internal>
2019-10-11 15:22:07 +0000

    opencart

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://www.modaroom.com/admin/');
+define('HTTP_CATALOG', 'https://www.modaroom.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.modaroom.com/admin/');
+define('HTTPS_CATALOG', 'https://www.modaroom.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/ssl/admin/');
+define('DIR_SYSTEM', '/var/www/ssl/system/');
+define('DIR_IMAGE', '/var/www/ssl/image/');
+define('DIR_STORAGE', '/var/www/storage/');
+define('DIR_CATALOG', '/var/www/ssl/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'munchmoda280384483082');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://www.modaroom.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://www.modaroom.com/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/ssl/catalog/');
+define('DIR_SYSTEM', '/var/www/ssl/system/');
+define('DIR_IMAGE', '/var/www/ssl/image/');
+define('DIR_STORAGE', '/var/www/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'munchmoda280384483082');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
